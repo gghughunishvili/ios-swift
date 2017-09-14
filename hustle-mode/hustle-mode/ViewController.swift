0362c33ba535025d9b126ea33c6804231878e13a
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2017-09-14 23:53:30 +0400

    initial commit

---
+++ hustle-mode/hustle-mode/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  hustle-mode
+//
+//  Created by Giorgi Ghughunishvili on 9/14/17.
+//  Copyright © 2017 Giorgi Ghughunishvili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("thats me dude")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
