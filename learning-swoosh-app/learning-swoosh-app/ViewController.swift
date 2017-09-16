298f3165162c5f30f5a854e02afc5d1f2464ea35
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2017-09-16 15:59:58 +0400

    new project, initial commit

---
+++ learning-swoosh-app/learning-swoosh-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  learning-swoosh-app
+//
+//  Created by Giorgi Ghughunishvili on 9/16/17.
+//  Copyright © 2017 Giorgi Ghughunishvili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
