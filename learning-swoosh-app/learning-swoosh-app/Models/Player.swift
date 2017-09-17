b9aece3854ff77a32f977fbb4169dfa9c19ab3c1
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2017-09-17 20:44:50 +0400

    added player model and passing data through views by segues

---
+++ learning-swoosh-app/learning-swoosh-app/Models/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  learning-swoosh-app
+//
+//  Created by Giorgi Ghughunishvili on 9/17/17.
+//  Copyright © 2017 Giorgi Ghughunishvili. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+}
