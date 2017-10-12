651ac9fbc3e88d1aa12f3c3111be8094f908acb0
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2017-10-12 13:43:48 +0400

    new project initial commit

---
+++ t-shopping/t-shopping/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  t-shopping
+//
+//  Created by Giorgi Ghughunishvili on 10/12/17.
+//  Copyright © 2017 Giorgi Ghughunishvili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
