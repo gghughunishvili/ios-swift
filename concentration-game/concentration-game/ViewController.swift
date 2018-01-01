cdffe380740eb92947fe140d89337905475ef752
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2018-01-01 13:56:42 +0400

    initial commit for Concentration Game from stanford

---
+++ concentration-game/concentration-game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  concentration-game
+//
+//  Created by Giorgi Ghughunishvili on 1/1/18.
+//  Copyright © 2018 Giorgi Ghughunishvili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
